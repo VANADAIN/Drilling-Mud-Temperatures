9d91e37501a9e3a185042bc463a17abd091d6180
Ivan Marchuk <ivan.marchuk@mail.ru>
2019-10-23 23:40:45 +0300

    init

---
+++ index.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const url = require('url')
+
+const {app, BrowserWindow, Menu} = require('electron')
+
+let win
+
+process.env.NODE_ENV = 'production'
+
+app.on('ready', () => {
+
+    win = new BrowserWindow({
+        width: 1000,
+        height: 700
+    })
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, '/index.html'),
+        prorocol: 'file:',
+        slashes: true
+    }))
+
+    win.on('closed', () => {
+        win = null
+    })
+
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+    Menu.setApplicationMenu(mainMenu)
+
+})
+
+const mainMenuTemplate = [
+    {
+        label: 'File'
+    }
+]
+
+app.on('window-all-closed', () => {
+    app.quit()
+})
