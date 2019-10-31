0e9ba6dc32aa349b407e6754637e85333c70cc9d
Ivan Marchuk <ussrivanko@gmail.com>
2019-10-31 15:41:28 +0300

    add to getter

--- getter.js
+++ getter.js
@@ -1,3 +1,25 @@
 
 // получение переменных из инпутов
 
+
+let consumption = document.getElementById('consumption').value
+
+let diameter = document.getElementById('diameter').value
+
+let depth = document.getElementById('depth').value
+
+let outer_d = document.getElementById('outer_d').value
+
+let inner_d = document.getElementById('inner_d').value
+
+let hydro_shaggily = document.getElementById('hydro_shaggily').value
+
+let connection = document.getElementById('connection').value
+
+let channel_d = document.getElementById('channel_d').value
+
+let length = document.getElementById('length').value
+
+let liquid_type = document.getElementById('liquid_type').value
+
+let density = document.getElementById('density').value
